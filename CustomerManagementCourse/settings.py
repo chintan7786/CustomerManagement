7227168936eb3110fac3bcf710ba4beffd752a05
chintan7786 <davdachintan97@gmail.com>
2021-05-31 13:36:56 +0530

    v2

--- CustomerManagementCourse/settings.py
+++ CustomerManagementCourse/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fx_r=waq3=c-3-8dzw+fje=-oudmp(dgk_qse$yu4!l$q@#2-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
